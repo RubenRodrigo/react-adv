ebad131c58c019ee042483f7b1ead3784b9e2175
Ruben Rodrigo <rodrigohde905@gmail.com>
2022-02-12 18:23:03 -0500

    Control Props alt

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { onChangeArgs, Product } from '../interfaces/interfaces';
 
 interface useProductArgs {
@@ -10,18 +10,15 @@ interface useProductArgs {
 export const useProduct = ({ product, onChange, value = 0 }: useProductArgs) => {
 
 	const [counter, setCounter] = useState(value);
-	const isControlled = useRef(!!onChange)
 
 	// Si el onChange es controlado (Es enviado)
 	// esta función no modificara el estado .
 	// Sera el useEffect el que lo modificara segun el value 
 	const increaseBy = (value: number) => {
-		if (isControlled.current) {
-			return onChange!({ count: value, product })
-		}
 
 		const newValue = Math.max(counter + value, 0)
 		setCounter(newValue)
+
 		onChange && onChange({ count: newValue, product })
 	}
 
